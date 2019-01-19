245ef4bcce4a7bbfe6fdad8785df1cffd618046c
Harouna Soumare <harounasoumare90@yahoo.fr>
2019-01-19 01:58:35 +0100

    finalize bugs on past code and pom file errors and make it compile

---
+++ supermarket-receipt/src/test/java/com/supermarketReceipt/AppTest.java
@@ -0,0 +1,19 @@
+package com.supermarketReceipt;
+
+
+import org.assertj.core.api.Assertions;
+import org.testng.annotations.Test;
+
+
+
+/**
+ * Unit test for simple App.
+*/
+public class AppTest
+{
+    @Test
+    public void testApp()
+    {
+        Assertions.assertThat(true).isTrue();
+    }
+}
