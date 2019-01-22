b04592cb41deb790a38590f998cb44dd2243f0ff
smohamedl <slaghlal@yahoo.fr>
2019-01-22 10:25:59 +0100

    discount test

--- src/test/java/fr/esiea/supermarket/model/SupermarketTest.java
+++ src/test/java/fr/esiea/supermarket/model/SupermarketTest.java
@@ -1,5 +1,6 @@
 package fr.esiea.supermarket.model;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SupermarketTest {
@@ -21,5 +22,35 @@ public class SupermarketTest {
         Receipt receipt = teller.checksOutArticlesFrom(cart);
 
         // Todo: complete this test
+
+
+    }
+
+    @Test
+    public void testDiscount(){
+        SupermarketCatalog catalog = new FakeCatalog();
+        Product toothbrush = new Product("toothbrush", ProductUnit.Each);
+        catalog.addProduct(toothbrush, 1.00);
+
+        Product oranges = new Product("orange", ProductUnit.Each);
+        catalog.addProduct(oranges, 2.00);
+
+        Discount discountToothbrush = new Discount(toothbrush, "For better teeth", 0.3);
+        Discount discountOranges = new Discount(oranges, "For better health", 0.5);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemQuantity(oranges, 3);
+        cart.addItemQuantity(toothbrush, 2);
+
+        Teller teller = new Teller(catalog);
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        receipt.addDiscount(discountOranges);
+        receipt.addDiscount(discountToothbrush);
+
+        double perceivedValue = receipt.getTotalPrice();
+        double expectedValue = 7.2;
+
+        Assertions.assertThat(perceivedValue).isEqualTo(expectedValue);
     }
 }
