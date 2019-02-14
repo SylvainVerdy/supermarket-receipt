e9c02df22b8959f4213c3e7685ff69f4353ec931
Harouna Soumare <harounasoumare90@yahoo.fr>
2019-02-14 22:54:42 +0100

    adding TestReceiptPrinter to codecov

---
+++ src/test/java/fr/esiea/supermarket/model/TestReceiptPrinter.java
@@ -0,0 +1,40 @@
+package fr.esiea.supermarket.model;
+
+
+import fr.esiea.supermarket.ReceiptPrinter;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class TestReceiptPrinter {
+
+    @Test
+    public void testReceiptPrinter(){
+        SupermarketCatalog catalog = new FakeCatalog();
+
+        Product toothbrush = new Product("toothbrush", ProductUnit.Each);
+        Product banana = new Product("banana",ProductUnit.Each);
+        Product apples = new Product("apples",ProductUnit.Each);
+
+        catalog.addProduct(toothbrush,1.00);
+        catalog.addProduct(banana,2.00);
+        catalog.addProduct(apples,1.50);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemQuantity(toothbrush, 2);
+        cart.addItemQuantity(banana,3);
+        cart.addItemQuantity(apples,3);
+
+        Teller teller = new Teller(catalog);
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+        ReceiptPrinter defaultReceiptPrinter = new ReceiptPrinter();
+
+        String expectedReceiptPrinter ="toothbrush                          2.00\n  1.00 * 2\nbanana                              6.00\n  2.00 * 3\napples                              4.50\n  1.50 * 3\n\nTotal:                             12.50";
+
+        Assertions.assertThat(expectedReceiptPrinter).isEqualTo(defaultReceiptPrinter.printReceipt(receipt));
+        Assertions.assertThat(receipt.getItems().size()).isEqualTo(3);
+        Assertions.assertThat(new ReceiptPrinter().printReceipt(receipt)).isNotBlank();
+    }
+
+
+}
