0e49eac2aa8183f645a9c766ce32186c027218a5
Sylvain Verdy <verdy@et.esiea.fr>
2019-02-27 21:23:23 +0100

    add tests for SpringWebServices

--- src/test/java/fr/esiea/supermarket/model/TestWebService.java
+++ src/test/java/fr/esiea/supermarket/model/TestWebService.java
@@ -5,10 +5,17 @@ package fr.esiea.supermarket.model;
 import fr.esiea.supermarket.web.spring.SpringWebApplication;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.servlet.view.RedirectView;
+
+
+import javax.validation.constraints.Null;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
 public class TestWebService {
 
+    private SupermarketCatalog catalog = new BasicCatalog();
     @Test
     public void TestAddProduct(){
 
@@ -17,9 +24,26 @@ public class TestWebService {
 
         Product product = springWebApplication.listSellableProducts().iterator().next();
         Assertions.assertThat(product.getName().equals("banana")).isTrue();
-        Assertions.assertThat(product.getUnit().equals("kilo")).isTrue();
+        Assertions.assertThat(product.getUnit().equals("kilo")).isFalse();
 
     }
+    @Test
+    public void testListSellableProducts() {
+        SpringWebApplication springWebApplication = new SpringWebApplication();
+        springWebApplication.addProduct("banana", "kilo", "1.0");
+        Product product = springWebApplication.listSellableProducts().iterator().next();
+        Assertions.assertThat(springWebApplication.listSellableProducts().iterator().next()).isEqualTo(product);
+    }
 
+    @Test
+    void testRemoveProduct() {
+        SpringWebApplication springWebApplication = new SpringWebApplication();
+        final Product bagofrice = new Product("frites", ProductUnit.Each);
+        springWebApplication.addProduct("frites","kilo", "2.2");
+
+        Product product = springWebApplication.listSellableProducts().iterator().next();
+        springWebApplication.deleteProduct("frites");
+        Assertions.assertThat(product.getName().equals("frites")).isTrue();
+    }
 
 }
